ddfbbd04c1a54826bd4faaf1bb470fe5a9b94165
szymonsliwa <szsliwicki@gmail.com>
2026-05-19 09:45:01 +0200

    Add files via upload

---
+++ pokazówka/waypoints.m
@@ -0,0 +1,8 @@
+% Example mission parameters
+mission_waypoints = [
+    0, 0;       % Start position
+    25, 0;     % First waypoint
+    25, 25;   % Second waypoint
+    0, 25;     % Third waypoint
+    0, 0        % Return home
+];
